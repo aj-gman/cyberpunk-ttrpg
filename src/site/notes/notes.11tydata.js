f7be45c61fdf40f4d86f08622ee7e9673f3dfd4b
aj-gman <aid3njacks@gmail.com>
2026-09-05 20:27:52 +0300

    Update files

--- src/site/notes/notes.11tydata.js
+++ src/site/notes/notes.11tydata.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: [".env.local", ".env"] });
+require("dotenv").config();
 const settings = require("../../helpers/constants");
 const { pickNoteMetadata } = require("../../helpers/bases-engine/noteMetadata");
 const pluginLoader = require("../../helpers/pluginLoader");
